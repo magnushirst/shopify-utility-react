00663aa9ead8296d733344b59f528f897bfd92d0
Magnus Hirst <magnushirst@gmail.com>
2021-04-09 19:35:55 +0100

    Allowing filter of namespace when fetching metafields

--- src/api/getMetafields.js
+++ src/api/getMetafields.js
@@ -1,17 +1,17 @@
 import {gql} from "graphql-tag";
 
 export const GET_METAFIELDS = gql`
-    query {
-        shop {
-            metafields(first: 10) {
-                edges {
-                    node {
-                        id
-                        namespace
-                        key
-                        value
-                    }
+    query($namespace: String!){
+    shop {
+        metafields( first: 20, namespace: $namespace) {
+            edges {
+                node {
+                id
+                namespace
+                key
+                value
                 }
             }
-        }
-    }`;
+        } 
+    }
+}`;
--- src/components/pages/metafields.js
+++ src/components/pages/metafields.js
@@ -1,14 +1,27 @@
-import React from "react"
+import React, {useState} from 'react';
 import BackChevron from "../atom/backChevron";
 import TableHeader from "../atom/tableHeader";
 import TableRow from "../atom/tableRow";
 import DataTable from "../molecule/dataTable";
 import {GET_METAFIELDS} from "../../api/getMetafields";
-import {useQuery} from "@apollo/client";
+import {useLazyQuery} from "@apollo/client";
 import {ShopifyGraphClient} from "../../api/shopifyGraphqlClient";
+import TextInput from "../atom/textInput";
 
-const Metafields = () =>{
-    const { loading, error, data } = useQuery(GET_METAFIELDS, {client: ShopifyGraphClient});
+const Metafields = () => {
+    const [namespaceInput, setNamespaceInput] = useState("");
+    const [namespace, setNamespace] = useState("");
+    const [getMetafields, {loading, error, data}] = useLazyQuery(GET_METAFIELDS, {
+            client: ShopifyGraphClient,
+            variables: {namespace}
+        }
+    );
+
+    function refreshData() {
+        setNamespace(namespaceInput);
+        getMetafields();
+    }
+
     return (
         <div className="about">
             <div className="heading">
@@ -19,12 +32,15 @@ const Metafields = () =>{
                 <div>An error occurred :(</div>
             }
             {loading &&
-            <div>...loading data</div>
+                <div>...loading data</div>
             }
+            <TextInput text={'namespace: '} name={'namespace'} id={'namespace'} value={namespaceInput}
+                       onChange={(e) => setNamespaceInput(e.target.value)}/>
+            <button onClick={refreshData} className={'u-mt-medium u-mb-medium thin-btn'}>Refresh</button>
             <DataTable>
-                <TableHeader headings={['ID', 'Name', 'Value', 'Namespace']} />
+                <TableHeader headings={['ID', 'Name', 'Value', 'Namespace']}/>
                 {data && data.shop.metafields.edges.map((edge) =>
-                    <TableRow fields={[edge.node.id, edge.node.key, edge.node.value, edge.node.namespace]} />
+                    <TableRow fields={[edge.node.id, edge.node.key, edge.node.value, edge.node.namespace]}/>
                 )}
             </DataTable>
         </div>
